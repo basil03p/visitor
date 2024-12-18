2f1b6a1fa45f2f6b9000b9177846434b9a1c7ce7
basil03p <basilmalikudy3@gmail.com>
2024-12-18 15:31:32 +0530

    initial

--- app/src/main/java/com/suii/visitorapp/MainActivity.java
+++ app/src/main/java/com/suii/visitorapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.suii.visitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.add);
+        b2=(Button) findViewById(R.id.search);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), addv.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), addv.class);
+                startActivity(i);
+            }
+        });
     }
 }
